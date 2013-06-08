ba8f5add7ae9d97723e614418c1777cd0bc88216
thesofmtodder <thesofmtodder@gmail.com>
2013-06-09 09:45:31 +1000

    Wrong commit redoing all code

---
+++ gen/com/thesoftmodder/api13/timetable/html5/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.thesoftmodder.api13.timetable.html5;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
