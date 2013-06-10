71cc576cb0a82261b52061e5b9e8cc9be3808c24
thesoftmodder <thesoftmodder@gmail.com>
2013-06-10 15:04:35 +1000

    Display Tweaking Many Features still not implemented

--- src/com/thesoftmodder/api13/timetable/html5/Main.java
+++ src/com/thesoftmodder/api13/timetable/html5/Main.java
@@ -21,6 +21,7 @@ package com.thesoftmodder.api13.timetable.html5;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
+//import android.view.WindowManager;
 
 public class Main extends DroidGap
 {
@@ -29,8 +30,11 @@ public class Main extends DroidGap
     {
         super.onCreate(savedInstanceState);
         // Set by <content src="index.html" /> in config.xml
+        //getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        //WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.loadUrl(Config.getStartUrl());
-        //super.loadUrl("file:///android_asset/www/index.html")
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
 
