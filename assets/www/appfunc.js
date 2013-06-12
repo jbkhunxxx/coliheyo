fdcf5d35cf41ce2a68c1f51c6fe1b2b882b6abae
thesoftmodder <thesoftmodder@gmail.com>
2013-06-12 21:53:29 +1000

    Add db

--- assets/www/appfunc.js
+++ assets/www/appfunc.js
@@ -3,11 +3,27 @@ function onLoad() {
 }
 var url = "https://raw.github.com/thesoftmodder/timetable-android-13-html5/downloads/upcode/updates.js"
 function onDeviceReady() {
+     var db = window.openDatabase("timetable", "1.0", "Timetable DB", 3000000);
      $.ajax({
   	url: url,
   	dataType: "script",
   	success: success
-     });
-     var db = window.openDatabase("timetable", "1.0", "Timetable DB", 3000000);
+     }); 
+     function populateDB(tx) {
+	       tx.executeSql('CREATE TABLE holidays (id INTEGER PRIMARY KEY, dates TEXT);');
+	       tx.executeSql('CREATE TABLE classes (id INTEGER PRIMARY KEY, lessid NUMERIC, week TEXT, day TEXT, room TEXT);');
+	       tx.executeSql('CREATE TABLE homework (id INTEGER PRIMARY KEY, lessid NUMERIC, duedate TEXT, homework BLOB);');
+	       tx.executeSql('CREATE TABLE holidays (id INTEGER PRIMARY KEY, dates TEXT);');
+     }
+     function errorCB(tx, err) {
+	       console.log("error");
+     }
+     function successCB() {
+               console.log("success");
+		alert("d");
+     }
+     db.transaction(populateDB, errorCB, successCB);
+
+
 }
 
