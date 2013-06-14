4b0079bb1ca1292be60537b552fab29ceb21f80f
thesoftmodder <thesoftmodder@gmail.com>
2013-06-14 17:24:20 +1000

    New Bugfixes

--- upcode/updates.js
+++ upcode/updates.js
@@ -1,38 +1,24 @@
 function onConfirm(button) {
     if (button==1){
-        navigator.app.loadUrl("https://github.com/thesoftmodder/timetable-android-13-html5/raw/downloads/app/v1_2.apk", {openExternal: true});
+        navigator.app.loadUrl("https://github.com/thesoftmodder/timetable-android-13-html5/raw/downloads/app/v1_11.apk", {openExternal: true});
     }
 }
 
-<<<<<<< HEAD
 if (appv<1.11){
     updateAvailable = true;
     //updateText = "App Update Available"
     navigator.notification.confirm(
-        'Updated App Contains New Features (v1.1.1)',  // message
+        'Updated App Contains Bugfixes (v1.1.1)',  // message
         onConfirm,              
         'App Update Available',           
         'Download,Later'          
     );
-    console.log("Latest Version");
+    console.log("Update Avail");
 }
-else if (appv==1.11){
-=======
-if (appv=="1.1"){
-    /*updateAvailable = true;
-    //updateText = "App Update Available"
-    navigator.notification.confirm(
-        'Updated App Contains New Features (v1.2)',  // message
-        onConfirm,              // callback to invoke with index of button pressed
-        'App Update Available',            // title
-        'Download,Later'          // buttonLabels
-    );*/
-    console.log("Latest Version");
-}
-else if (appv=="1.2"){
->>>>>>> ba981cafa6bfa1b126143cd5542063f40f758a26
-    updateAvailable = false;
+if (appv==1.11){
+    updateAvailable = true;
     updateText = "latest"
+    console.log("Latest Version");
 }
 
 
