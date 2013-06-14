502c297f7a42dd6c6b746a83ed0532cb3f169bc6
thesoftmodder <thesoftmodder@gmail.com>
2013-06-14 17:16:32 +1000

    New Version Bugfixes

--- upcode/updates.js
+++ upcode/updates.js
@@ -1,20 +1,26 @@
-//updated for 2.8.1
-
 function onConfirm(button) {
     if (button==1){
         navigator.app.loadUrl("https://github.com/thesoftmodder/timetable-android-13-html5/raw/downloads/app/v1_2.apk", {openExternal: true});
     }
 }
 
-if (appVersion=="1.1"){
+if (appv<1.11){
     updateAvailable = true;
-    alert("COME UPDATE");
-    updateText = "An update is available. Bugfixes New Features."
+    //updateText = "App Update Available"
+    navigator.notification.confirm(
+        'Updated App Contains New Features (v1.1.1)',  // message
+        onConfirm,              
+        'App Update Available',           
+        'Download,Later'          
+    );
+    console.log("Latest Version");
 }
-else if (appVersion=="1.2"){
+else if (appv==1.11){
     updateAvailable = false;
     updateText = "latest"
 }
+
+
 
 
 
