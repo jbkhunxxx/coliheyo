f5bff831e45895c968c2f88db62430f38751f3df
thesoftmodder <thesoftmodder@gmail.com>
2013-06-27 22:04:08 +1000

    New Update

--- upcode/updates.js
+++ upcode/updates.js
@@ -1,28 +1,30 @@
 function onConfirm(button) {
     if (button==1){
-        navigator.app.loadUrl("https://github.com/thesoftmodder/timetable-android-13-html5/raw/downloads/app/v1_11.apk", {openExternal: true});
+        navigator.app.loadUrl("https://github.com/thesoftmodder/timetable-android-13-html5/raw/downloads/app/v1_22.apk", {openExternal: true});
     }
 }
 
-if (appv<1.11){
+if (appv<1.22){
     updateAvailable = true;
     //updateText = "App Update Available"
     navigator.notification.confirm(
-        'Updated App Contains Bugfixes (v1.1.1)',  // message
+        'Removed Debug. Added homework feature! (v1.22)',  // message
         onConfirm,              
         'App Update Available',           
-        'Download,Later'          
+        'Download,Dismiss'          
     );
     console.log("Update Avail");
 }
-if (appv==1.11){
+if (appv==1.22){
     updateAvailable = true;
     updateText = "latest"
     console.log("Latest Version");
 }
 
-
-
-
+if (appv>1.22){
+    updateAvailable = true;
+    updateText = "latest"
+    console.log("U must be the software Programmer");
+}
 
 
